f024ed1e7da1b98b65ce25a4585dcf368f548848
Quoc An <tranan498@gmail.com>
2023-08-23 20:24:15 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,9 @@
 import { RouterView } from "vue-router";
 import { localDB } from "./utils/localDB";
 import NavBar from "./components/NavBar.vue";
-import { supabase } from "./constants/supabase";
-import {
-  NLoadingBarProvider,
-  darkTheme,
-  lightTheme,
-  NConfigProvider,
-} from "naive-ui";
+
+import { NLoadingBarProvider, NConfigProvider } from "naive-ui";
 import { onMounted, ref } from "vue";
-
-const theme = ref(localStorage.theme === "dark" ? darkTheme : lightTheme);
-
-const handleToggleTheme = () => {
-  const currentTheme = theme.value.name;
-  theme.value = currentTheme === "light" ? darkTheme : lightTheme;
-  localStorage.theme = theme.value.name;
-};
 
 const themeOverrides = {
   Collapse: {
@@ -28,13 +15,6 @@ const themeOverrides = {
 onMounted(() => {
   const { init } = localDB();
   init();
-  supabase.auth.getSession().then(({ data }) => {
-    session.value = data.session;
-  });
-
-  supabase.auth.onAuthStateChange((_, _session) => {
-    session.value = _session;
-  });
 });
 </script>
 
